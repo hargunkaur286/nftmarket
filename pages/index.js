a031242a1000da53f6fbf1f5f5ade3f39e511d47
Kashish Jiandani <kashishjiandani@gmail.com>
2022-11-17 22:58:35 +0530

    firebase linked

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {db} from '../firebase'
+import { onValue, ref, set } from "firebase/database";
 
 function index() {
+console.log("helloooo")
+onValue(ref(db,'/'),(snapshot)=>{
+  const data = snapshot.val();
+  console.log(data);
+});
+
   return (
     <div>
-      
+      kashish
     </div>
   )
 }
