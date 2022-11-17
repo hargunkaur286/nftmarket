b5987d29400b8b3adaac7ed988fc445193324748
hargunkaur286 <hargunkaur286@gmail.com>
2022-11-17 21:34:05 +0530

    Added Frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+//INTERNAL IMPORT
+import NavBar from '../components/componentsindex';
 
-export default MyApp
+const MyApp = ({Component, pageProps})=> (
+    <div>
+    <NavBar />
+    <Component {...pageProps} />
+</div>
+);
+
+
+
+
+export default MyApp;
