593fe512b57420eb0a1dd8161645e5db177493e2
Kashish Jiandani <kashishjiandani@gmail.com>
2022-11-18 21:04:48 +0530

    hello

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -24,8 +24,12 @@ const HeroSection = () => {
             your NTFs and sell them
           </p>
           <Button
-            btnName="Start your search"
+            btnName="Vote Now"
             handleClick={() => router.push("/searchPage")}
+          />
+          <Button
+          btnName="Create Petition"
+          handleClick={() => router.push("/searchPage")}
           />
         </div>
         <div className={Style.heroSection_box_right}>
