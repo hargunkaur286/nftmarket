3dfe89ad8d9835f7c6eb4222daebc147d2a2f76b
vanshika <vgoel6900@gmail.com>
2022-11-18 23:57:58 +0530

    oakifj

--- components/Collection/Collection.js
+++ components/Collection/Collection.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { onValue, ref, set } from "firebase/database";
 import {
   BsFillAlarmFill,
   BsFillCalendarDateFill,
   BsCalendar3,
 } from "react-icons/bs";
+
+import { db } from '../../firebase'
 
 //INTERNAL IMPORT
 import Style from "./Collection.module.css";
@@ -14,101 +17,21 @@ const Collection = () => {
   const [popular, setPopular] = useState(true);
   const [following, setFollowing] = useState(false);
   const [news, setNews] = useState(false);
+  const [apiData, setApiData] = useState([])
+  useEffect(() => {
+    let arr = [];
+    onValue(ref(db, `/`), (snapshot) => {
+      const data = snapshot.val();
+      const x = Object.values(data);
+      x.forEach(e => {
+        Object.values(e).forEach(a => arr.push(a))
+      })
+      // console.log(arr, 'arr');
+      setApiData(arr);
+    })
+  }, []);
 
-  const CardArray = [
-    {
-      background: images.creatorbackground1,
-      user: images.user1,
-    },
-    {
-      background: images.creatorbackground2,
-      user: images.user2,
-    },
-    {
-      background: images.creatorbackground3,
-      user: images.user3,
-    },
-    {
-      background: images.creatorbackground4,
-      user: images.user4,
-    },
-    {
-      background: images.creatorbackground5,
-      user: images.user5,
-    },
-    {
-      background: images.creatorbackground6,
-      user: images.user6,
-    },
-    {
-      background: images.creatorbackground7,
-      user: images.user7,
-    },
-    {
-      background: images.creatorbackground8,
-      user: images.user8,
-    },
-  ];
-  const newsArray = [
-    {
-      background: images.creatorbackground3,
-      user: images.user3,
-    },
-    {
-      background: images.creatorbackground4,
-      user: images.user4,
-    },
-    {
-      background: images.creatorbackground5,
-      user: images.user5,
-    },
-    {
-      background: images.creatorbackground6,
-      user: images.user6,
-    },
-    {
-      background: images.creatorbackground1,
-      user: images.user1,
-    },
-    {
-      background: images.creatorbackground2,
-      user: images.user2,
-    },
-  ];
-  const followingArray = [
-    {
-      background: images.creatorbackground1,
-      user: images.user1,
-    },
-    {
-      background: images.creatorbackground2,
-      user: images.user2,
-    },
-    {
-      background: images.creatorbackground3,
-      user: images.user3,
-    },
-    {
-      background: images.creatorbackground4,
-      user: images.user4,
-    },
-    {
-      background: images.creatorbackground5,
-      user: images.user5,
-    },
-    {
-      background: images.creatorbackground6,
-      user: images.user6,
-    },
-    {
-      background: images.creatorbackground7,
-      user: images.user7,
-    },
-    {
-      background: images.creatorbackground8,
-      user: images.user8,
-    },
-  ];
+
 
   const openPopular = () => {
     if (!popular) {
@@ -153,7 +76,7 @@ const Collection = () => {
       </div>
       {popular && (
         <div className={Style.collection_box}>
-          {CardArray.map((el, i) => (
+          {apiData.map((el, i) => (
             <DaysComponent key={i + 1} i={i} el={el} />
           ))}
         </div>
@@ -161,7 +84,7 @@ const Collection = () => {
 
       {following && (
         <div className={Style.collection_box}>
-          {followingArray.map((el, i) => (
+          {apiData.map((el, i) => (
             <DaysComponent key={i + 1} i={i} el={el} />
           ))}
         </div>
@@ -169,7 +92,7 @@ const Collection = () => {
 
       {news && (
         <div className={Style.collection_box}>
-          {newsArray.map((el, i) => (
+          {apiData.map((el, i) => (
             <DaysComponent key={i + 1} i={i} el={el} />
           ))}
         </div>
